8c939fe300a7da4b05f45a77ba94d80ab3f05f44
Marius W <weitzel.marius@gmail.com>
2015-01-03 17:45:11 +0100

    Speichern & Laden geschrieben

---
+++ Aloha-Uebung/Location.swift
@@ -0,0 +1,24 @@
+//
+//  Location.swift
+//  Aloha-Uebung
+//
+//  Created by Medien on 03.01.15.
+//  Copyright (c) 2015 Medien. All rights reserved.
+//
+
+import Foundation
+
+
+public struct Location {
+    //ID bestehend aus der Koordinate
+    var lat: NSNumber
+    var long: NSNumber
+    //Name des Punkts
+    var name: String
+    
+    init() {
+        lat = 0
+        long = 0
+        name = ""
+    }
+}
